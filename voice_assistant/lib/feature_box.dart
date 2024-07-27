23b076fe4e0825d74ab996d0971764991a846247
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-07-27 19:22:40 +0530

    added speech to text features

--- voice_assistant/lib/feature_box.dart
+++ voice_assistant/lib/feature_box.dart
@@ -4,10 +4,12 @@ import 'package:voice_assistant/pallete.dart';
 class FeatureBox extends StatelessWidget {
   final Color color;
   final String headerText;
+  final String descriptionText;
   const FeatureBox({
     super.key,
     required this.color,
     required this.headerText,
+    required this.descriptionText,
   });
 
   @override
@@ -20,18 +22,42 @@ class FeatureBox extends StatelessWidget {
           Radius.circular(15),
         ),
       ),
-      child: Column(
-        children: [
-          Text(
-            headerText,
-            style: const TextStyle(
-              fontFamily: 'Cera Pro',
-              color: Pallete.blackColor,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 20.0,
+          left: 15,
+          bottom: 20,
+        ),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                headerText,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 3,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(right:20),
+              child: Text(
+                descriptionText,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- voice_assistant/lib/home_page.dart
+++ voice_assistant/lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 import 'package:voice_assistant/feature_box.dart';
 import 'package:voice_assistant/pallete.dart';
 
@@ -10,15 +12,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+  
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    bool available = await speechToText.initialize();
+    print('Speech-to-text initialized: $available');
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    print('Started listening');
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    print('Stopped listening');
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+    print('Recognized words: $lastWords');
+  }
+
+  @override
+  void dispose() {
+    speechToText.stop();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Allen'),
-          leading: const Icon(Icons.menu),
-          centerTitle: true,
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: const Text('Allen'),
+        leading: const Icon(Icons.menu),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           children: [
             //Virtual assistant picture
             Stack(
@@ -59,8 +102,8 @@ class _HomePageState extends State<HomePage> {
                   border: Border.all(
                     color: Pallete.borderColor,
                   ),
-                  borderRadius:
-                      BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+                  borderRadius: BorderRadius.circular(20)
+                      .copyWith(topLeft: Radius.zero)),
               child: const Padding(
                 padding: EdgeInsets.symmetric(vertical: 10.0),
                 child: Text(
@@ -92,15 +135,44 @@ class _HomePageState extends State<HomePage> {
             ),
 
             // features list
-            Column(
+            const Column(
               children: [
                 FeatureBox(
                   color: Pallete.firstSuggestionBoxColor,
                   headerText: 'ChatGPT',
+                  descriptionText:
+                      'A smarter way to say organized and informed with ChatGPT',
+                ),
+                FeatureBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: 'Dall-E',
+                  descriptionText:
+                      'Get inspired and stay creative with your personal assistant powered by Dall-E',
+                ),
+                FeatureBox(
+                  color: Pallete.thirdSuggestionBoxColor,
+                  headerText: 'Smart Voice Assistant',
+                  descriptionText:
+                      'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
                 ),
               ],
             )
           ],
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            await stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
+        child: const Icon(Icons.mic),
+      ),
+    );
   }
 }
